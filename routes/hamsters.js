5d93149a4118566d47118c776755605efc33ab76
email-90 <70515786+email-90@users.noreply.github.com>
2021-04-20 13:40:17 +0200

    Fixed Get, Post, put, delete, and get /random and get /:id

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -4,14 +4,73 @@ const router = express.Router()
 const dbFunction = require('../database.js');
 const db = dbFunction();
 
-let allHamsters = [];
 router.get('/', async (req, res) => {
 	const colRef = await db.collection('hamsters').get();
+	let allHamsters = [];
 	colRef.forEach(docRef => {
 		let hamster = docRef.data();
 		allHamsters.push(hamster);
 	});
 	res.send(allHamsters);
+});
+
+router.get('/random', async (req, res) => {
+
+	const colRef = await db.collection('hamsters').get();
+	let allHamsters = [];
+	colRef.forEach(docRef => {
+		let hamster = docRef.data();
+		allHamsters.push(hamster);
+	});
+	let index = parseInt(allHamsters.length);
+	let randomIndex = Math.floor(Math.random() * (index - 1));
+	res.send(allHamsters[randomIndex]);
+})
+
+router.get('/:id', async (req, res) => {
+	const id = req.params.id;
+	if (!id) {
+		res.sendStatus(400);
+	}
+	const response = await db.collection('hamsters').doc(id).get();
+	data = response.data();
+	res.send(data)
+});
+
+
+router.post('/', async (req, res) => {
+	const data = req.body;
+
+	//Fixa så att datan blir rätt...
+	if( !data ) {
+		res.sendStatus(400);
+		return
+	}
+	let response = await db.collection('hamsters').add(data);
+	res.status(200).send(response.id);
+});
+
+router.put('/:id', async (req, res) => {
+	let id = req.params.id;
+	let data = req.body;
+
+	//testat !data i if satsen men den fungerar ej..
+	if (!id) {
+		res.sendStatus(404);
+		return
+	}
+	await db.collection('hamsters').doc(id).set(data, {merge: true})
+	res.sendStatus(200);
+	
+});
+
+router.delete('/:id', async (req, res) => {
+	const id = req.params.id; 
+	if(!id) {
+		res.send(404);
+	}
+	await db.collection('hamsters').doc(id).delete()
+	res.sendStatus(200);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,18 +5,24 @@ const hamsters = require('./routes/hamsters.js');
 const path = require('path');
 const cors = require('cors');
 
-//middleware
+//Middleware
 app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-	console.log(req.method);
+	console.log(req.method, req.url);
 	next();
 });
 
+//Static folders
+app.use(express.static('./images/hamsters'));
+
+
+//Routes
 app.use('/hamsters', hamsters);
 
 
+//Start server
 app.listen(PORT, () => {
 	console.log('Server is listening to port ' + PORT);
 });
